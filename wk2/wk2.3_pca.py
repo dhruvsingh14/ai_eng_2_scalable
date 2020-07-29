924bcc9a998b78cd13f4eb75c2a28b8aadb2bc50
dhruvsingh14 <42729611+dhruvsingh14@users.noreply.github.com>
2020-07-28 20:16:56 -0400

    Add files via upload

--- wk2/wk2.3_pca.py
+++ wk2/wk2.3_pca.py
@@ -1,6 +1,6 @@
-######################
-# Week 2.2: Plotting #
-######################
+#################
+# Week 2.3: PCA #
+#################
 
 ################################
 # Checking System Requirements #
@@ -49,10 +49,10 @@ df.createOrReplaceTempView('washing')
 
 print(df.show())
 
-#############################
-#  #
-#############################
-
+################################################
+# Selecting all the relevant fields of inquiry #
+################################################
+# restricting our query to max and mins of each field
 result = spark.sql("""
 SELECT * from (
     SELECT
@@ -84,12 +84,73 @@ AND min_speed is not null
 AND max_speed is not null
 """)
 
+# you shouldn't see any nulls in results dataframe since it excludes nulls
+# from its calculation
+print(result.show())
+
+# checking for null rows:
+# by subtracting count of total rows - count with nulls excluded
 print(df.count() - result.count())
 
+####################
+# SparkML Pipeline #
+####################
 # importing libraries
 from pyspark.ml.feature import PCA
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
+
+# compiling all features into one column
+assembler = VectorAssembler(inputCols=result.columns, outputCol="features")
+
+# transforming single column
+features = assembler.transform(result)
+
+# checking what the combined column looks like
+print(features.rdd.map(lambda r : r.features).take(10))
+
+# declaring pca object, with 3 clusters, and fitting it to single column
+pca = PCA(k=3, inputCol="features", outputCol="pcaFeatures")
+model = pca.fit(features)
+
+# transforming single column - using pca object
+result_pca = model.transform(features).select("pcaFeatures")
+result_pca.show(truncate=False)
+# of the 3 columns, the middle column has a different sign from our example
+# may still plot correctly, only along the other side of the axis.
+
+# printing row count - SAME
+print(result_pca.count())
+
+# sampling columns
+rdd = result_pca.rdd.sample(False,0.8)
+
+# drawing into lists, and preparing to plot
+x = rdd.map(lambda a: a.pcaFeatures).map(lambda a : a[0]).collect()
+y = rdd.map(lambda a: a.pcaFeatures).map(lambda a : a[1]).collect()
+z = rdd.map(lambda a: a.pcaFeatures).map(lambda a : a[2]).collect()
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection = '3d')
+
+ax.scatter(x,y,z, c='r', marker = 'o')
+
+ax.set_xlabel('dimension1')
+ax.set_ylabel('dimension2')
+ax.set_zlabel('dimension3')
+
+plt.show()
+
+
+
+
+
+
+
 
 
 
